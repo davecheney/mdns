6c5be0d5f54041bfffb0b6c0b92dc94c63ac6e73
Dave Cheney <dave@cheney.net>
2011-08-20 18:08:08 +1000

    Added mdns listener

--- zeroconf.go
+++ zeroconf.go
@@ -1,7 +1,11 @@
 package zeroconf
 
 import (
+	"fmt"
+	"log"
 	"net"
+	
+	dns "github.com/miekg/godns"
 )
 
 var (
@@ -12,8 +16,49 @@ var (
 	}
 )
 
+func openIPv4Socket() *net.UDPConn {
+	conn, err := net.ListenUDP("udp4", &net.UDPAddr{
+		IP:   net.IPv4zero,
+		Port: 5353,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}	
+	return conn
+}
+
 func init() {
 	go Registry.mainloop()
+
+	socket := openIPv4Socket()	
+	if err := socket.JoinGroup(nil, net.IPv4(224, 0, 0, 251)); err != nil {
+		log.Fatal(err)
+	}
+
+	listener := &listener {
+		socket: socket,
+		registry: Registry,
+	}
+	go listener.mainloop()
+}
+
+func mcastInterfaces() []net.Interface {
+	ifaces := make([]net.Interface, 0)
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, i := range interfaces {
+		if isMulticast(i) {
+			fmt.Printf("%#v\n", i)
+			ifaces = append(ifaces, i)
+		}
+	}
+	return ifaces
+}
+
+func isMulticast(i net.Interface) bool {
+	return (i.Flags & net.FlagUp > 0) && (i.Flags & net.FlagMulticast > 0)
 }
 
 type Service struct {
@@ -39,5 +84,23 @@ func (r *registry) mainloop() {
 		case s := <- r.add:
 			r.services = append(r.services, s)
 		}
+	}
+}
+
+type listener struct {
+	socket *net.UDPConn
+	registry *registry
+}	
+
+func (l *listener) mainloop() {
+	buf := make([]byte, 1500)
+	for {
+		read, err := l.socket.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		msg := &dns.Msg{}
+		msg.Unpack(buf[:read])
+		log.Println(msg.String())
 	}
 }
