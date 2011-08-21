c9adc902234f943b7009b7abfeb897dda6dfaf8c
Dave Cheney <dave@cheney.net>
2011-08-21 20:53:40 +1000

    Basic *Service message parsing

--- service.go
+++ service.go
@@ -1,6 +1,7 @@
 package zeroconf
 
 import (
+	"strings"
 	"net"
 
 	dns "github.com/miekg/godns"
@@ -16,9 +17,17 @@ type Service struct {
 
 // s.unmarshal may not be complete, return false if so
 func (s *Service) valid() bool {
-	return true
+	return len(s.Name) > 0
 }
 
 func (s *Service) unmarshal(msg *dns.Msg) {
-
+	for i := range msg.Answer {
+		switch rr := msg.Answer[i].(type) {
+		case *dns.RR_SRV:
+			x := strings.Split(rr.Hdr.Name, ".")
+			s.Name = x[0]
+			s.Type = strings.Join(x[1:3], ".")
+			s.Domain = strings.Join(x[3:], ".")
+		}
+	}
 }
