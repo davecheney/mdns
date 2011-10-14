96f28234dee76dc74b96c960fe50158da610f607
Dave Cheney <dave@cheney.net>
2011-10-14 17:56:07 +1100

    Cleaned up browser

--- examples/mdns-browser/browser.go
+++ examples/mdns-browser/browser.go
@@ -2,30 +2,15 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	dns "github.com/miekg/godns"
 	"github.com/davecheney/mdns"
 )
 
-var (
-	questions = []dns.Question{
-		{"_ssh._tcp.local.", dns.TypeANY, dns.ClassINET},
-	}
-)
-
 func main() {
 	results := mdns.Subscribe(dns.TypeANY)
 	for {
-		select {
-		case <-time.After(2e9):
-			for _, q := range questions {
-				msg := new(dns.Msg)
-				msg.SetQuestion(q.Name, q.Qtype)
-				// zone.Broadcast <- msg
-			}
-		case result := <-results:
-			fmt.Printf("+ %-32s%32s %s\n", result.Name(), result.Type(), result.Domain())
-		}
+		result := <-results
+		fmt.Printf("+ %-32s%32s %s\n", result.Name(), result.Type(), result.Domain())
 	}
 }
