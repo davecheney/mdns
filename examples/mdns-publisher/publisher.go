097a30550ad987dc71448135490b44b20e3d4de0
Dave Cheney <dave@cheney.net>
2011-10-14 17:52:58 +1100

    A slightly better example

--- examples/mdns-publisher/publisher.go
+++ examples/mdns-publisher/publisher.go
@@ -7,15 +7,25 @@ import (
 )
 
 func main() {
-	mdns.PublishA("stora.local.", 3600, net.IPv4(192, 168, 1, 200))
-	mdns.PublishPTR("_ssh._tcp.local.", 3600, "stora._ssh._tcp.local.")
-	mdns.PublishSRV("stora._ssh._tcp.local.", 3600, "stora.local.", 22)
-	mdns.PublishTXT("stora._ssh._tcp.local.", 3600, "")
+	// A simple example. Publish an A record for my router at 192.168.1.254.
 
 	mdns.PublishA("router.local.", 3600, net.IPv4(192, 168, 1, 254))
-	mdns.PublishPTR("_ssh._tcp.local.", 3600, "router._ssh._tcp.local.")
-	mdns.PublishSRV("router._ssh._tcp.local.", 3600, "router.local.", 22)
-	mdns.PublishTXT("router._ssh._tcp.local.", 3600, "")
+
+	// A more compilcated example. Publish a SVR record for ssh running on port
+	// 22 for my home NAS.
+
+	// Publish an A record as before
+        mdns.PublishA("stora.local.", 3600, net.IPv4(192, 168, 1, 200))
+
+	// Publish a PTR record for the _ssh._tcp DNS-SD type
+        mdns.PublishPTR("_ssh._tcp.local.", 3600, "stora._ssh._tcp.local.")
+
+	// Publish a SRV record typing the _ssh._tcp record to an A record and a port.
+        mdns.PublishSRV("stora._ssh._tcp.local.", 3600, "stora.local.", 22)
+
+	// Most mDNS browsing tools expect a TXT record for the service even if there
+	// are not records defined by RFC 2782.
+        mdns.PublishTXT("stora._ssh._tcp.local.", 3600, "")
 
 	<-make(chan bool)
 }
