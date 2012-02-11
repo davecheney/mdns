713366d22ab2931c205333a356defb0838887f09
Dave Cheney <dave@cheney.net>
2012-02-12 09:40:41 +1100

    Publish records via Publish

--- examples/mdns-publisher/publisher.go
+++ examples/mdns-publisher/publisher.go
@@ -1,28 +1,35 @@
 package main
 
 import (
+	"log"
 	"github.com/davecheney/mdns"
 )
 
+func mustPublish(rr string){
+	if err := mdns.Publish(rr) ; err != nil {
+		log.Fatalf(`Unable to publish record "%s": %v`, rr, err)
+	}
+}
+
 func main() {
 	// A simple example. Publish an A record for my router at 192.168.1.254.
-	mdns.Publish("router.local. 3600 A 192, 168, 1, 254")
+	mustPublish("router.local. 60 IN A 192.168.1.254")
 
 	// A more compilcated example. Publish a SVR record for ssh running on port
 	// 22 for my home NAS.
 
 	// Publish an A record as before
-	mdns.Publish("stora.local. 3600 A 192, 168, 1, 200")
+	mustPublish("stora.local. 60 IN A 192.168.1.200")
 
 	// Publish a PTR record for the _ssh._tcp DNS-SD type
-	mdns.Publish("_ssh._tcp.local. 3600 PTR stora._ssh._tcp.local.")
+	mustPublish("_ssh._tcp.local. 60 IN PTR stora._ssh._tcp.local.")
 
 	// Publish a SRV record tying the _ssh._tcp record to an A record and a port.
-	mdns.Publish("stora._ssh._tcp.local. 3600 SRV stora.local. 22")
+	mustPublish("stora 60 IN SRV 0 0 22 stora.local.")
 
 	// Most mDNS browsing tools expect a TXT record for the service even if there
 	// are not records defined by RFC 2782.
-	mdns.Publish(`stora._ssh._tcp.local. 3600 ""`)
+	mustPublish(`stora._ssh._tcp.local. 60 IN TXT ""`)
 
 	select {}
 }
